121735a06f17b97e827d9f908c4a5f8464c0b023
brucef0rte <bfrte71@gmail.com>
2025-03-30 19:09:31 -0500

    so much. lots of cleanup. Is basically a working alpha-ish at this point. Needs way more generator options and heavy formatting 4 reading

--- main.py
+++ main.py
@@ -1,49 +1,6 @@
 import random
 import os
-'''
-Mushroom Hunter Game
-Practice for Codecademy "Learn Object Oriented Programming with Python" class
-Goal: You are on a mission to find new edible mushrooms for your tribe. Find mushrooms,
-    test them, try not to die.
--Player starts with 10 HP
--If a player is poisoned they lose 1 HP per round
--When player searches for mushroom:
-    Present 3 randomly generated mushrooms
-    Player can examine or eat each of three mushrooms
 
-    A mushroom name is made of 2 parts from 2 lists
-Classes: Player, Mushroom
-
-Player:
-    attributes:
-        HP
-            if HP == 0: gameOver
-        isPoisoned
-            if True -1 HP per round
-        hadVision
-            set True if psychedlic method called
-    methods:
-        searchForMushroom
-            returns 3 mushrooms to pick from, uses 1 round
-        examineMushroom
-            check for tell-tale signs of a poisonous mushroom, uses 1/2(?) rounds
-        chooseMushroom
-            pick 1 of the three mushroom to eat, then eat it
-
-Mushroom:
-    attributes:
-        name: comprised of random selection from 2 dictionaries (key "name": value "description)
-        appearance: description of mushroom based on key values for name
-        healthEffects:
-            healthy - restore 1 health
-            poisonous - sets isPoisoned condition
-            psychedelic - restore 1 health, cure poison, lose 1 day (cosmetic)
-    methods:
-        heal
-        inflictPoison
-        psychedelic
-
-'''
 # Create our Player Class
 class Player:
     def __init__(self, hp=10, isPoisoned = False, hadVision = False):
@@ -51,15 +8,14 @@ class Player:
         self.isPoisoned = isPoisoned
         self.hadVision = hadVision
         self.mushrooms = []
-
+        self.satchel = []
+    # this will be returned when we print our player
     def __str__(self):
         object = f"Your HP is {self.hp}. "
         if self.isPoisoned == True:
             object += f"You are poisoned.\n"
         if self.hadVision == True:
             object += f"You had a vision.\n"
-        if self.hp <= 0:
-            object += f"You are dead.\n"
         elif self.hp > 0 and self.hp <= 5:
             object += "You're knocking on death's door.\n"
         elif self.hp > 5 and self.hp <= 8:
@@ -68,7 +24,13 @@ class Player:
             object += "You're in great shape.\n"
         else:
             object += "You're in perfect shape.\n"
+        object += f"Your satchel has {len(self.satchel)} mushrooms in it. Just {5 - len(self.satchel)} to go."
         return object
+    # check if we're dead or not
+    def checkDead(self):
+        if self.hp <= 1:
+            print('you ded')
+            exit
     # generate 3 mushrooms for our player to pick from
     def searchForMushroom(self):
         if self.hp > 10:
@@ -77,11 +39,14 @@ class Player:
         if self.isPoisoned:
             self.hp -= 1
             print(f"Oof. The poison rends your tummy for -1 hp.\n Current HP: {self.hp} / 10")
+        self.checkDead()
         self.mushrooms = [Mushroom() for _ in range(3)]
         print("You found 3 mushrooms!")
-        for i, mushroom in enumerate(self.mushrooms):
-            print(mushroom)
+        print(f"Mushroom A: {self.mushrooms[0]}")
+        print(f"Mushroom B: {self.mushrooms[1]}")
+        print(f"Mushroom C: {self.mushrooms[2]}")
     
+    # use a,b,c or A,B,C to pick a mushroom
     def pickMushroom(self):
         self.choice = input("Which mushroom would you like to try A, B, C? ").upper()
         if self.choice == "A":
@@ -92,15 +57,16 @@ class Player:
             self.eatMushroom(self.mushrooms[2])
         else:
             print("Invalid choice. Please pick A, B, or C.")
-
+    # eat the mushroom and see what happens. Healthy mushrooms can be taken home, and restore 1 health. Poison mushrooms take 1 hp at the beigning of each round.
+    # psychedlic mushrooms remove poison, and set a hadVision flag to true
     def eatMushroom(self, mushroom):
         if mushroom.isNeutral == True:
-            print(f"You ate the {mushroom.name} and it was fine. Nothing happened.\n\n")
-            self.isHealthy = True
+            print(f"You ate the {mushroom.name}. It's delicious and nutritious. You add it to your satchel.\n\n")
+            self.satchel.append(mushroom)
+            self.hp = self.hp + 1
         elif mushroom.isPoisoin == True:
             print(f"You ate the {mushroom.name} and it was poisonous!\n\n")
             self.isPoisoned = True
-            self.hp -= 1
         elif mushroom.isPsychedelic == True:
             print(f"You ate the {mushroom.name} and it was psychedelic!\n\n")
             self.hadVision = True
@@ -108,22 +74,25 @@ class Player:
                 print("You feel the poison leaving your body.\n")
             self.isPoisoned = False
         else:
-            print("You ate a regular mushroom. Nothing happened.\n\n")
-        if self.hp <= 0:
-            print("you ded.")
-            exit
+            print("This mushroom only hurt your tastebuds. Yuck!\n\n")
+        self.checkDead()
 
 # Create Mushroom class
 class Mushroom:
+    # lists of properties for random generation. Need to greatly expanded.
     colors = ["blue", "green", "cyan", "spotted grey"]
     shapes = ["bell", "hexagonal", "translucent", "bulbous", "square"]
     names1 = ["destroying", "memsmerizing", "sickening", "delectable"]
     names2 = ["angel", "puppy", "catgirl", "oyster", "magic", "shiitake"]
+
+    # constructor to randomly set variables for mushroom type. Need to fix all the Poisoin typos, and make sure only 1 of the variables is set per mushroom.
     def __init__(self):
         self.name = random.choice(self.names1) + " " + random.choice(self.names2)
         self.isNeutral = random.choice([True, False])
-        self.isPoisoin = random.choice([True, False])
-        self.isPsychedelic = random.choice([True, False])
+        if self.isNeutral == False:
+            self.isPoisoin = random.choice([True, False])
+            if self.isPoisoin == False:
+                self.isPsychedelic = random.choice([True, False])
         self.color = random.choice(self.colors)
         self.shape = random.choice(self.shapes)
         self.appearance = f"a {self.color} colored, {self.shape} shaped mushroom"
@@ -139,9 +108,12 @@ class Mushroom:
         return object
 
 player = Player()
-while player.hp > 0:
+while True:
+    player.checkDead()
+    if len(player.satchel) >= 5:
+        print("You've gathered enough supplies from the tribe. Return home. You've earned your rest.")
+        if player.hadVision == True:
+            print("You bring back with you an apocalyptic vision of a world sized mushroom hurtling through space.")
+        break
     player.searchForMushroom()
     player.pickMushroom()
-    if player.hp <= 0:
-        print("you ded.")
-        exit
